3775b42c54e8c512310ad199e7a852b91f0970ac
Rodrigo Clarindo Alves <rcalves@ZAPL10447>
2018-05-04 16:33:31 -0300

    Aula 02-05-18

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,11 +1,15 @@
 #coding: utf-8
 
+import random
+
 print('********************************')
 print('Bem vindo ao jogo de Advinhação!')
 print('********************************')
 
-numero_secreto = 36
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
+
+print(numero_secreto)
 
 for rodada  in range(1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
